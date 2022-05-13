9ccd1774eaf24559bc713016e3e295a681de0de5
Prudence1234 <prudencebosibori@gmail.com>
2022-05-13 14:25:10 +0300

    formui

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Insert into database</title>
+    </head>
+    <body>
+        <form action="" method="">
+            <label>Name</label><input type="text" name="name"><br>
+
+            <label>Email</label><input type="email" name="email"><br>
+
+            <label>Age</label><input type="text" name="age"><br>
+
+            <label>Address</label><input type="text" name="address"><br>
+
+            <button type="submit" name="submit">Submit</button>
+        </form>
+    </body>
+</html>
